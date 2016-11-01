c8ed9b150121d05f80ad5d9c4512f8741c88fc6c
Anil Rayamajhi <anilrm07@gmail.com>
2016-11-01 09:51:43 -0700

    packages added

--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
 var
+  dotenv = require('dotenv').load({silent: true}),
   express = require('express'),
   app = express(),
   logger = require('morgan'),
   mongoose = require('mongoose'),
   bodyParser = require('body-parser'),
+  request = require('request'),
   Yelp = require('yelp'),
+  server = require('http').createServer(app),
   // apiRoutes = require('./routes/cars.js'),
   PORT = process.env.port || 7000
 
 
   var yelp = new Yelp({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    consumer_key: process.env.YELP_CONSUMER_KEY,
+    consumer_secret: process.env.YELP_CONSUMER_SECRET,
+    token: process.env.YELP_ACCESS_TOKEN,
+    token_secret: process.env.YELP_ACCESS_TOKEN_SECRET
   })
 
 // mongoose.connect('mongodb://localhost/factories-practice', function(err) {
@@ -26,18 +29,17 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 // app.use(express.static('client'))
 
-app.use('/api', function(req, res){
-  res.json({message: "YO YO YO"})
+app.get('/api', function(req, res){
+  yelp.business('caressa-beauty-salon-culver-city', function(err, data) {
+    if (err) return console.log(error);
+    res.send(data)
+  });
 })
 
 // app.get('*', function(req, res) {
 //   res.sendFile('/client/index.html', {root: './'})
 // })
-yelp.business('caressa-beauty-salon-culver-city', function(err, data) {
-  if (err) return console.log(error);
-  console.log(data);
-});
 
-app.listen(PORT, function(err) {
+server.listen(PORT, function(err) {
   console.log(err || "Server running on port " + PORT)
 })

4762643ac233400777882d412442f53788e1a199
Anil Rayamajhi <anilrm07@gmail.com>
2016-11-02 18:32:35 -0700

    template work on progress

--- client/controllers.js
+++ client/controllers.js
@@ -5,6 +5,7 @@ angular.module('myApp')
   .controller('registerController', registerController)
   .controller('pageController', pageController)
   .controller('singlePageController', singlePageController)
+  .filter('tel', tel)
   // .filter('pageFilter', pageFilter)
 
 
@@ -121,5 +122,51 @@ function singlePageController($state, $http, $stateParams){
   $http.get('api/pages/'+$stateParams.id)
     .success(function(page){
       vm.page = page;
+      vm.id = $stateParams.id;
     })
 }
+
+function tel(){
+return function (tel) {
+   if (!tel) { return ''; }
+
+   var value = tel.toString().trim().replace(/^\+/, '');
+
+   if (value.match(/[^0-9]/)) {
+       return tel;
+   }
+
+   var country, city, number;
+
+   switch (value.length) {
+       case 10:
+           country = 1;
+           city = value.slice(0, 3);
+           number = value.slice(3);
+           break;
+
+       case 11:
+           country = value[0];
+           city = value.slice(1, 4);
+           number = value.slice(4);
+           break;
+
+       case 12:
+           country = value.slice(0, 3);
+           city = value.slice(3, 5);
+           number = value.slice(5);
+           break;
+
+       default:
+           return tel;
+   }
+
+   if (country == 1) {
+       country = "";
+   }
+
+   number = number.slice(0, 3) + '-' + number.slice(3);
+
+   return (country + " (" + city + ") " + number).trim();
+};
+};
